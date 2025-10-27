a83679ceb4209767efc8e9de98a665e1f04c3add
lcrol77 <logan.croley.18@gmail.com>
2025-10-26 22:34:12 -0400

    feat: implemented minigit init

---
+++ cmd/minigit_cli/internal/repo/repo.go
@@ -0,0 +1,17 @@
+package repo
+
+import "os"
+
+func Init() error {
+	err := os.Mkdir(".minigit", 0755)
+	if err != nil {
+		return err
+	}
+
+	os.Mkdir(".minigit/blobs", 0755)
+	os.Mkdir(".minigit/commits", 0755)
+	os.Mkdir(".minigit/refs", 0755)
+	os.WriteFile(".minigit/HEAD", []byte("refs/heads/main"), 0644)
+
+	return nil 
+}
--- cmd/minigit_cli/main.go
+++ cmd/minigit_cli/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"minigit/cmd/minigit_cli/internal/repo"
 	"os"
 )
 
+const (
+	INIT = "init"
+)
+
 func main(){
-	fmt.Println(len(os.Args), os.Args)
+	if len(os.Args) <= 1{
+		fmt.Println("start a working area")
+		fmt.Println("\tinit \tCreate an empty Mini Git repository or reinitialize an existing one")
+	}
+	cmd := os.Args[1]
+	switch cmd {
+		case INIT:
+			repo.Init()
+	}
 }
